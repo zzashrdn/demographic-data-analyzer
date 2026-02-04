998914823c08d58fcd8a88cdd3e6ae8c4ee37dfc
Izzah Saharudin <126446014+zzashrdn@users.noreply.github.com>
2026-02-04 15:11:08 +0800

    Create test_module.py

---
+++ test_module.py
@@ -0,0 +1,60 @@
+import unittest
+import demographic_data_analyzer
+
+class DemographicAnalyzerTestCase(unittest.TestCase):
+    def setUp(self):
+        self.data = demographic_data_analyzer.calculate_demographic_data(print_data=False)
+
+    def test_race_count(self):
+        actual = self.data['race_count'].tolist()
+        expected = [27816, 3124, 1039, 311, 271]
+        self.assertEqual(actual, expected)
+
+    def test_average_age_men(self):
+        actual = self.data['average_age_men']
+        expected = 39.4
+        self.assertAlmostEqual(actual, expected, places=1)
+
+    def test_percentage_bachelors(self):
+        actual = self.data['percentage_bachelors']
+        expected = 16.4
+        self.assertAlmostEqual(actual, expected, places=1)
+
+    def test_higher_education_rich(self):
+        actual = self.data['higher_education_rich']
+        expected = 46.5
+        self.assertAlmostEqual(actual, expected, places=1)
+
+    def test_lower_education_rich(self):
+        actual = self.data['lower_education_rich']
+        expected = 17.4
+        self.assertAlmostEqual(actual, expected, places=1)
+
+    def test_min_work_hours(self):
+        actual = self.data['min_work_hours']
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_rich_percentage(self):
+        actual = self.data['rich_percentage']
+        expected = 10.0
+        self.assertAlmostEqual(actual, expected, places=1)
+
+    def test_highest_earning_country(self):
+        actual = self.data['highest_earning_country']
+        expected = 'Iran'
+        self.assertEqual(actual, expected)
+
+    def test_highest_earning_country_percentage(self):
+        actual = self.data['highest_earning_country_percentage']
+        expected = 41.9
+        self.assertAlmostEqual(actual, expected, places=1)
+
+    def test_top_IN_occupation(self):
+        actual = self.data['top_IN_occupation']
+        expected = 'Prof-specialty'
+        self.assertEqual(actual, expected)
+
+
+def test(function):
+    unittest.main(exit=False)
